5e1a81094a8b0547d7d75097ab796a3449d3a4c8
Daniel Bola <dbolamartinez@gmail.com>
2026-06-02 21:16:57 +0200

    :building_construction: Creada la estructura lógica. Ya podemos ver las categorías y los codigos de estado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import codeStatus from './status_codes.json'
 
 function App() {
+  console.log(codeStatus);
+  console.log(Array.isArray(codeStatus));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {codeStatus.map(category => (
+        <ul>
+          <div className='App-header'>
+            <h1>{category.category}</h1>
+            <p>{category.categoryDescription}</p>
+          </div>
+          <div>
+            {category.codes.map(code => (
+              <div>
+                <img src={code.image}></img>
+                <h2>{code.code}</h2>
+                <h3>{code.name}</h3>
+                <p>{code.description}</p>
+              </div>
+            ))}
+          </div>
+        </ul>
+      ))}
     </div>
   );
 }
